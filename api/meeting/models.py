2938ce1132a08b6c32ff977bc99110f35f39aa08
Bruno DIAS <Bruno DIAS>
2017-10-10 13:37:43 -0300

    Alterações no app de reuniões

--- api/meeting/models.py
+++ api/meeting/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+MEETING_TYPE = (
+	('conselho-executivo', 'Conselho Executivo'),
+	('outro', 'Outros')
+)
 class Meeting(models.Model):
 	title = models.CharField('Título da ata de reunião', max_length=500)
 	description = models.TextField(verbose_name='Descrição', null=True, blank=True)
 	ppt = models.FileField(upload_to='apresentacao_reuniao/%Y/%m/%d/', null=False, blank=False)
 	pdf = models.FileField(upload_to='ata_reuniao/%Y/%m/%d/', null=False, blank=False)
 	date = models.DateField('Data da Ata de reunião', null=True, blank=True)
-	meetingType = models.CharField('Tipo de reuniao', max_length=500)
+	meetingType = models.CharField('Tipo de reuniao', max_length=500, choices=MEETING_TYPE)
 
 	created_at  = models.DateTimeField('Criado em', auto_now_add=True)
 	updated_at  = models.DateTimeField('Atualizado em', auto_now=True)
